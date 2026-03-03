92797c70da7d9574b77db8474e34ccb12d03bb70
abhay pandit <abhaypanditzx@gmail.com>
2026-03-03 19:24:15 +0530

    url changes

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ connectCloudinary();
 //middlewares
 app.use(express.json());
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://resturant-mern-sigma.vercel.app/",
   credentials:true
 }));
 app.use(cookieParser());
