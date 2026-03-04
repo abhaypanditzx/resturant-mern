637ef2a1f795a9e2f2aa911edb30b2362bf22593
abhay pandit <abhaypanditzx@gmail.com>
2026-03-04 10:06:07 +0530

    switch to production

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -11,7 +11,7 @@ const generateToken = (res, payload) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure:true,
     sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000,
   });
@@ -113,7 +113,7 @@ const loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure:true,
       sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
 
