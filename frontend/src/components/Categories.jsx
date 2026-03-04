6a55097575e8798b8fe578aeaa854932259c5873
abhay pandit <abhaypanditzx@gmail.com>
2026-03-04 17:09:31 +0530

    category styling fixes

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -8,11 +8,11 @@ const Categories = () => {
   return (
     <section className=" py-16 from-gray-50 to-white ">
       <div className="container mx-auto px-4 text-center ">
-        <h2 className="tex-4xl font-bold mb-4">
+        <h2 className="text-4xl font-bold mb-4">
           Explore Our <span className="text-yellow-500">Categories</span>
         </h2>{" "}
         <p>Discover delicious dishes from our carefully created categories</p>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
+        <div className="grid grid-cols-2 py-3te sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
           {categories.map((item) => (
             <div key={item._id} onClick={()=>navigate("/")} className="cursor-pointer group">
               <div className="relative">
@@ -21,8 +21,6 @@ const Categories = () => {
                     className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-300" />
                     </div>
                     <div className="absolute inset-0 w-32 h-32 mx-auto bg-linear-to-t rounded-full  from-black/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"> </div>
-
-
               </div>
               <div className="mt-4">
                 <h3 className="text-2xl font-semibold text-gray-800 group-hover:text-yellow-500 transition-colors duration-300">{item.name}</h3>
